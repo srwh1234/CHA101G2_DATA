ec9fc0f1b9d3e2b701efb0dc15e4f705d09a1a8a
pycccc <py7chang@gmail.com>
2023-05-27 15:02:53 +0800

    前台後台會員中心

--- 網頁/前台/js/my_favorite.js
+++ 網頁/前台/js/my_favorite.js
@@ -23,13 +23,13 @@ camera.addEventListener('click', function () {
 });
 // <!-- 右邊會員資料--票券收藏 -->
 //已完成訂單的假資料
-// const valid = {
-//     url: "https://www.kkday.com/zh-tw/product/142053-thredbo-resort-ski-snowboard-day-tour-from-haymarket-parramatta-australia",
-//     imgUrl: "https://scitechvista.nat.gov.tw/FileDownload/Article/20230330152155448044257.jpg",
-//     title: "票券收藏1",
-//     Number: "#0012",
-//     realPrice: 400,
-// };
+const valid = {
+  url: "https://www.kkday.com/zh-tw/product/142053-thredbo-resort-ski-snowboard-day-tour-from-haymarket-parramatta-australia",
+  imgUrl: "https://scitechvista.nat.gov.tw/FileDownload/Article/20230330152155448044257.jpg",
+  title: "票券收藏1",
+  summary: "簡介簡介簡介簡介簡介簡介簡介簡介簡介",
+  realPrice: 400,
+};
 //未完成訂單的假資料
 const valid2 = {
   url: "https://www.kkday.com/zh-tw/product/115643-limited-time-offer-2-4-meals-hsinchu-camping-xiong-glamping-taiwan",
@@ -38,62 +38,78 @@ const valid2 = {
   summary: "簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介簡介",
   realPrice: 1200,
 };
+//簡介超過超過100個字以"..."取代
+$(function () {
+  var len = 100;
+  $(".Number").each(function (i) {
+    if ($(this).text().length > len) {
+      $(this).attr("title", $(this).text());
+      var text = $(this).text().substring(0, len - 1) + "...";
+      $(this).text(text);
+    }
+  });
+});
+
 //存進sessionStorage
 sessionStorage.setItem('ticket_favorite', JSON.stringify(valid2));
+sessionStorage.setItem('ticket_favorite', JSON.stringify(valid));
 //一載入頁面就出現"
 const orderIncomplete = JSON.parse(sessionStorage.getItem('ticket_favorite'));
-$("#orderselectdiv").append(generateTicketItem(orderIncomplete));
-//"未完成訂單"和"已完成訂單"切換
-// $("#orderselect").on("change", function (e) {
-//     $(".ticket_item_class").remove();
-//     var selectedOption = $(this).val();
-//     var newTicketItem = "";
-//     if (selectedOption === "未完成訂單") {
-//         const orderIncomplete = JSON.parse(sessionStorage.getItem('ticket_favorite'));
-//         newTicketItem = generateTicketItem(orderIncomplete);
-//     }
-//     if (newTicketItem !== "") {
-//         $(this).after(newTicketItem);
-//         sessionStorage.setItem("ticketAdded", "false");
-//     }
-// });
-function generateTicketItem(order) {
-  return `
-      <div class="ticket_item_class">
-        <a href=${order.url} class="orderurl">
-          <div class="item_img_class">
-            <img src=${order.imgUrl} class="item_img">
-          </div>
-          <div class="item_commend_class">
-          <button type="button" class="close" aria-label="Close"  style=" border: 1px solid rgb(180, 174, 174)">
-            <span aria-hidden="true">&times;移除收藏</span>
-          </button>
-          </div>
-          <div class="item_content">
-            <h1 class="item_title">${order.title}</h1>
-            <div>
-               <p class="Number">${order.summary}</p>
-            </div>
-            <div>
-              <p class="price">TWD</p>
-              <p class="realPrice">${order.realPrice}</p>
-            </div>
-          </div>
-        </a>
-      </div>
-    `;
+// $("#orderselectdiv").append(generateTicketItem(orderIncomplete));
+
+$(".nav-item")
+  .eq(0)
+  .on("click", function () {
+    $(".tab-pane").eq(0).addClass("show active"); // 顯示
+    // 呼叫這個顯示行程卡片列表
+    getAiFavorite();
+  });
+
+function getAiFavorite() {
+  $.ajax({
+    url: "/getAiFavorite/" + memberId,
+    method: "GET",
+    dataType: "json",
+    success: function (aiFavorite) {
+      console.log("接收資料");
+      console.log(aiFavorite);
+
+      for (let i = 0; i < aiFavorite.length; i++) {
+        $(".tab-pane").eq(0).find("#orderselect")
+          .after(function generateTicketItem(order) {
+            return `
+                <div class="ticket_item_class">
+                  <a href=${order.url} class="orderurl">
+                    <div class="item_img_class">
+                      <img src=${order.imgUrl} class="item_img">
+                    </div>
+                    <div class="item_commend_class">
+                    <button type="button" class="remove_btn" aria-label="Close"  style=" border: 1px solid rgb(180, 174, 174)">
+                      <span aria-hidden="true">&times;移除收藏</span>
+                    </button>
+                    </div>
+                    <div class="item_content">
+                      <h1 class="item_title">${order.title}</h1>
+                      <div class="box">
+                         <p class="Number">${order.summary}</p>
+                      </div>
+                      <div>
+                        <p class="price">TWD</p>
+                        <p class="realPrice">${order.realPrice}</p>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              `;
+          })
+      }
+    },
+
+  });
 }
 
 
 // <!-- 右邊會員資料--旅遊團訂單 -->
-//已完成訂單的假資料
-const valid3 = {
-  url: "https://www.kkday.com/zh-tw/product/142053-thredbo-resort-ski-snowboard-day-tour-from-haymarket-parramatta-australia",
-  imgUrl: "https://blog.asiayo.com/wp-content/uploads/67817447_xl-1.jpg",
-  title: "已完成旅遊團TEST",
-  Number: "#0012",
-  realPrice: 1234,
-};
 //未完成訂單的假資料
 const valid4 = {
   url: "https://www.kkday.com/zh-tw/product/115643-limited-time-offer-2-4-meals-hsinchu-camping-xiong-glamping-taiwan",
@@ -103,41 +119,23 @@ const valid4 = {
   realPrice: 5678,
 };
 //存進sessionStorage
-sessionStorage.setItem('test_group_done', JSON.stringify(valid3));
+// sessionStorage.setItem('test_group_done', JSON.stringify(valid3));
 sessionStorage.setItem('test_group_incomplete', JSON.stringify(valid4));
 
 //一載入頁面在"未完成訂單上"
 const test_group_incomplete = JSON.parse(sessionStorage.getItem('test_group_incomplete'));
 $("#group_orderselectdiv").append(generateGroupOrderItem(test_group_incomplete));
 
-//"未完成訂單"和"已完成訂單"切換（旅遊團訂單）
-$("#group_orderselect").on("change", function (e) {
-  $(".group_order_item_class").remove();
-  var selectedOption = $(this).val();
-  var newGroupOrderItem = "";
 
-  if (selectedOption === "已完成訂單") {
-    const groupOrderDone = JSON.parse(sessionStorage.getItem('test_group_done'));
-    newGroupOrderItem = generateGroupOrderItem(groupOrderDone);
-  } else if (selectedOption === "未完成訂單") {
-    const groupOrderIncomplete = JSON.parse(sessionStorage.getItem('test_group_incomplete'));
-    newGroupOrderItem = generateGroupOrderItem(groupOrderIncomplete);
-  }
-
-  if (newGroupOrderItem !== "") {
-    $("#group_orderselectdiv").append(newGroupOrderItem);
-    sessionStorage.setItem("groupOrderAdded", "false");
-  }
-});
 function generateGroupOrderItem(groupOrder) {
   return `
-      <div class="group_order_item_class">
-        <a href=${groupOrder.url} class="group_order_url">
+      <div class="ticket_item_class">
+        <a href=${groupOrder.url} class="orderurl">
           <div class="item_img_class">
             <img src=${groupOrder.imgUrl} class="item_img">
           </div>
           <div class="item_commend_class">
-          <button type="button" class="close" aria-label="Close"  style=" border: 1px solid rgb(180, 174, 174)">
+          <button type="button" class="remove_btn" aria-label="Close"  style=" border: 1px solid rgb(180, 174, 174)">
           <span aria-hidden="true">&times;移除收藏</span>
         </button>
           </div>
@@ -156,15 +154,24 @@ function generateGroupOrderItem(groupOrder) {
       </div>
       `;
 }
+//移除資料
+const remove_btn = document.querySelector(".remove_btn");
 
+remove_btn.addEventListener("click", function () {
+  document.querySelector(".orderurl").removeAttribute("href");
+  document.querySelector(".ticket_item_class").remove();
+  // document.querySelector(".ticket_item_class").remove();
+});
+
+// =================================================================
 // 設定點擊AI行程規劃
 $(".nav-item")
-    .eq(3) // 選第四個
-    .on("click", function () {
-      $(".tab-pane").eq(3).addClass("show active"); // 顯示
-      // 呼叫這個顯示行程卡片列表
-      getAiFavorite();
-    });
+  .eq(3) // 選第四個
+  .on("click", function () {
+    $(".tab-pane").eq(3).addClass("show active"); // 顯示
+    // 呼叫這個顯示行程卡片列表
+    getAiFavorite();
+  });
 
 // 接收後台AI行程資料
 console.log(memberId);
@@ -179,7 +186,7 @@ function getAiFavorite() {
 
       for (let i = 0; i < aiFavorite.length; i++) {
         $(".tab-pane").eq(3).find("#group_orderselect")
-            .after(`<div class="group_order_item_class">
+          .after(`<div class="group_order_item_class">
           <div class="card-header">
             <div class="card-top">
               <h5 class="text-center">
